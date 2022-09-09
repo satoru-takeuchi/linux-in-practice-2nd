799c372b5319d211007159df35c63b78d78c23a1
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2022-09-09 13:09:38 +0000

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- 02-process-management-1/intignore.py
+++ 02-process-management-1/intignore.py
@@ -2,7 +2,7 @@
 
 import signal
 
-# SIGINTシグナルを無視するように設定。第1引数にはハンドラを設定するシグナルの番号(ここではsignal.SIGINT)を、
+# SIGINTシグナルを無視するように設定。第1引数にはハンドラを設定するシグナル(ここではsignal.SIGINT)を、
 # 第2引数にはシグナルハンドラ(ここではsignal.SIG_IGN)を指定する。
 signal.signal(signal.SIGINT, signal.SIG_IGN)
 
